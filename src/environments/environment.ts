da3e4c13547f5dd1028237765fc02cb1d04e6b89
Hany.Ahmed <hany.ahmed@fawry.com>
2020-12-30 14:18:35 +0200

    local profile

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  userApiBaseUrl: 'http://localhost:4402/api',
+  userApiBaseUrl: 'http://localhost:4402',
   testingEnvironmentLoginUrl : 'https://atfawry.fawrystaging.com/merchant/login',
   liveEnvironmentLoginUrl : 'https://atfawry.com/merchant/login',
   liveEnvironmentHost : 'atfawry.com',
