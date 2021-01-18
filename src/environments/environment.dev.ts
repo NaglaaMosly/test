56a91cee205ade4522721e943596747689e4b09b
Hany.Ahmed <hany.ahmed@fawry.com>
2021-01-18 21:14:21 +0200

    update configurations

---
+++ src/environments/environment.dev.ts
@@ -0,0 +1,8 @@
+export const environment = {
+    production: false,
+    userApiBaseUrl: window.location.origin + '/user-api',
+    testingEnvironmentLoginUrl : 'https://atfawry.fawrystaging.com/merchant/login',
+    liveEnvironmentLoginUrl : 'https://atfawry.com/merchant/login',
+    liveEnvironmentHost : 'atfawry.com',
+  };
+  
