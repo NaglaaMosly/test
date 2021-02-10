fd1b8ad9b22f2797152c6c86f4b14a4e97c939bd
Naglaa Reda <Naglaa.Reda@Fawry.com>
2021-02-10 12:07:10 +0200

    Updated environment.alpha.ts

--- src/environments/environment.alpha.ts
+++ src/environments/environment.alpha.ts
@@ -2,6 +2,9 @@ export const environment = {
    production: true,  
    beApiBaseUrl: window.location.origin + '/be-api' ,  
    userApiBaseUrl: window.location.origin + '/user-api',  
-   loginAppUrl: window.location.origin + '/be-login',  
+   loginAppUrl: window.location.origin + '/be-login',
+   testingEnvironmentLoginUrl : 'https://atfawry.fawrystaging.com/merchant/login',
+   liveEnvironmentLoginUrl : 'https://atfawry.com/merchant/login',
+   liveEnvironmentHost : 'atfawry.com',  
    securityOff: false
 };
