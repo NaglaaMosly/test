17a5f90db033cd31f6ddc3feb466cc1dd57dccb1
Naglaa Reda <Naglaa.Reda@Fawry.com>
2021-02-14 19:40:48 +0200

    Updated environment.develop.ts

---
+++ src/environments/environment.develop.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: false,
+  userApiBaseUrl: 'http://localhost:4402',
+  testingEnvironmentLoginUrl : 'https://atfawry.fawrystaging.com/merchant/login',
+  liveEnvironmentLoginUrl : 'https://atfawry.com/merchant/login',
+  liveEnvironmentHost : 'atfawry.com',
+};
