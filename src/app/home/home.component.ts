ee67297225cd89a23332105f7b74521e4bef2f8f
Samir.Ghoniem <samir.ghoniem@fawry.com>
2021-03-01 17:39:03 +0200

    refactor header

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,41 +1,39 @@
-import { ArabicStyleLoaderService } from './../shared/arabic-style-loader.service';
-import { Constants } from './../shared/constants';
 import { Component } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
 import { AuthService } from './../auth/auth.service';
+import { ArabicStyleLoaderService } from './../shared/arabic-style-loader.service';
+import { Constants } from './../shared/constants';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+	selector: 'app-home',
+	templateUrl: './home.component.html',
+	styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
 
-  applications;
-  currentUser: string;
+	applications;
 
-  constructor(public translate: TranslateService,
-              private authService: AuthService,
-              private arabicStyleLoader: ArabicStyleLoaderService) {}
+	constructor(public translate: TranslateService,
+		private authService: AuthService,
+		private arabicStyleLoader: ArabicStyleLoaderService) { }
 
-  changeLocal() {
-    this.translate.currentLang === Constants.ENGLISH_LOCAL_ID ? this.arabicStyleLoader.load() : this.arabicStyleLoader.unload();
-  }
+	changeLocal() {
+		this.translate.currentLang === Constants.ENGLISH_LOCAL_ID ? this.arabicStyleLoader.load() : this.arabicStyleLoader.unload();
+	}
 
-  logout() {
-    console.log("LOG OUT");
-    this.authService.logout();
-  }
+	logout() {
+		this.authService.logout();
+	}
 
-  get loggedInUserName() {
-    return this.authService?.getLoggedInUserName();
-  }
+	get loggedInUserName() {
+		return this.authService?.getLoggedInUserName();
+	}
 
-  get accessibleApplications() {
-    return this.authService.getAccessibleApplication();
-  }
+	get accessibleApplications() {
+		return this.authService.getAccessibleApplication();
+	}
 
-  isLoggedIn() {
-    return this.authService.isUserLoggedIn();
-  }
+	isLoggedIn() {
+		return this.authService.isUserLoggedIn();
+	}
 }
