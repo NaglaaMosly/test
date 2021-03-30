4bb90290fb7993c265564aa1592d93adf1a07a27
naglaa.reda <naglaa.reda@fawry.com>
2021-03-30 23:15:39 +0200

    sonar integration

---
+++ sonar-project.js
@@ -0,0 +1,11 @@
+const sonarqubeScanner = require('sonarqube-scanner');
+sonarqubeScanner(
+  {
+    serverUrl: 'http://10.95.0.181:31500',
+    options: {
+      'sonar.sources': '.',
+      'sonar.inclusions': 'src/**',
+    },
+  },
+  () => {}
+);
